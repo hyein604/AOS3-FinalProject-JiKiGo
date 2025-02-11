60550ee239184ee253c377a18ecd999a4aba1029
zzangse1 <zzangse1014@gmail.com>
2025-02-11 16:53:17 +0900

    Refactor: 초기화면 설정

--- app/src/main/java/com/protect/jikigo/LoginFragment.kt
+++ app/src/main/java/com/protect/jikigo/LoginFragment.kt
@@ -5,17 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.protect.jikigo.databinding.FragmentLoginBinding
+import com.protect.jikigo.databinding.FragmentSignUpFirstBinding
 
 
-class LoginFragment : Fragment() {
-
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
