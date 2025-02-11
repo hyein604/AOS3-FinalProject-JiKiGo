6e534a78b0dc0cf12fa311fc4ff5ed796f2b7326
hyeinLion <hyein604@naver.com>
2025-02-11 15:16:40 +0900

    Rename: fragment_today_news 에서 fragment_news_environment_all

--- app/src/main/java/com/protect/jikigo/NewsEnvironmentAllFragment.kt
+++ app/src/main/java/com/protect/jikigo/NewsEnvironmentAllFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TodayNewsFragment.newInstance] factory method to
+ * Use the [NewsEnvironmentAllFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TodayNewsFragment : Fragment() {
+class NewsEnvironmentAllFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class TodayNewsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today_news, container, false)
+        return inflater.inflate(R.layout.fragment_news_environment_all, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class TodayNewsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment TodayNewsFragment.
+         * @return A new instance of fragment TodayNewsAllFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TodayNewsFragment().apply {
+            NewsEnvironmentAllFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
