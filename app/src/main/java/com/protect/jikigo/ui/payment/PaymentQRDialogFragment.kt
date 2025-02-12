a8903e0879744291ae813c8c537bc66bdd3b2853
zzangse1 <zzangse1014@gmail.com>
2025-02-12 16:45:09 +0900

    Feat: binding 설정

---
+++ app/src/main/java/com/protect/jikigo/ui/payment/PaymentQRDialogFragment.kt
@@ -0,0 +1,31 @@
+package com.protect.jikigo.ui.payment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.protect.jikigo.databinding.FragmentPaymentQrDialogBinding
+
+
+class PaymentQRDialogFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentPaymentQrDialogBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPaymentQrDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
