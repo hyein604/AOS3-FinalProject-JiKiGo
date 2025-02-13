241b81627983faa0517b79a57aa3c02db2486aa0
zzangse1 <zzangse1014@gmail.com>
2025-02-13 10:38:15 +0900

    Refactor: 상속 변경

--- app/src/main/java/com/protect/jikigo/ui/reward/AttendBottomSheetFragment.kt
+++ app/src/main/java/com/protect/jikigo/ui/reward/AttendBottomSheetFragment.kt
@@ -1,22 +1,31 @@
 package com.protect.jikigo.ui.reward
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.protect.jikigo.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.protect.jikigo.databinding.FragmentAttendBottomSheetBinding
 
 
-class AttendBottomSheetFragment : Fragment() {
-
+class AttendBottomSheetFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentAttendBottomSheetBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_attend_bottom_sheet, container, false)
+    ): View {
+        _binding = FragmentAttendBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
