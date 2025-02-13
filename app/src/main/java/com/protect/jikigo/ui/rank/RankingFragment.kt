b20d2fc2a47dc06d0e6f549e13a6a8cc375a7465
hyeinLion <hyein604@naver.com>
2025-02-13 13:40:34 +0900

    Feat: 랭킹 페이지 이동

--- app/src/main/java/com/protect/jikigo/ui/rank/RankingFragment.kt
+++ app/src/main/java/com/protect/jikigo/ui/rank/RankingFragment.kt
@@ -5,15 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.protect.jikigo.R
-
+import com.protect.jikigo.databinding.FragmentRankingBinding
+import com.protect.jikigo.ui.rank.dialog.RankingHelpDialog
 
 class RankingFragment : Fragment() {
+
+    private var _binding: FragmentRankingBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ranking, container, false)
+    ): View {
+        _binding = FragmentRankingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 도움말 아이콘 클릭 시 다이얼로그 표시
+        binding.ivRankingHelp.setOnClickListener {
+            val dialog = RankingHelpDialog()
+            dialog.show(childFragmentManager, "RankingHelpDialog")
+        }
     }
 }
--- app/src/main/java/com/protect/jikigo/ui/reward/RewardFragment.kt
+++ app/src/main/java/com/protect/jikigo/ui/reward/RewardFragment.kt
@@ -5,16 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.protect.jikigo.R
+import com.protect.jikigo.databinding.FragmentHomeBinding
+import com.protect.jikigo.databinding.FragmentRewardBinding
+import com.protect.jikigo.ui.home.HomeFragmentDirections
 
 class RewardFragment : Fragment() {
-
+    private var _binding: FragmentRewardBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reward, container, false)
+    ): View {
+        _binding = FragmentRewardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setLayout()
+    }
+
+    private fun setLayout() {
+        moveToRanking()
+    }
+    private fun moveToRanking() {
+        binding.btnRewardWalkRankMore.setOnClickListener {
+            val action = RewardFragmentDirections.actionNavigationRewardToRanking()
+            findNavController().navigate(action)
+        }
     }
 }
