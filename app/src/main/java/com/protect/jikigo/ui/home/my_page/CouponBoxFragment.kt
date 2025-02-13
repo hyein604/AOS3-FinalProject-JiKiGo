96af8b59179534225b36272543dec0cd405cb202
zzangse1 <zzangse1014@gmail.com>
2025-02-13 18:02:00 +0900

    Fix: 바텀 네비게이션 설정

--- app/src/main/java/com/protect/jikigo/ui/home/my_page/CouponBoxFragment.kt
+++ app/src/main/java/com/protect/jikigo/ui/home/my_page/CouponBoxFragment.kt
@@ -1,22 +1,44 @@
 package com.protect.jikigo.ui.home.my_page
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.protect.jikigo.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.protect.jikigo.databinding.FragmentCouponBoxBinding
 
 
 class CouponBoxFragment : Fragment() {
-
+    private var _binding: FragmentCouponBoxBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coupon_box, container, false)
+    ): View {
+        _binding = FragmentCouponBoxBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setLayout()
+    }
+
+    private fun setLayout() {
+        onClickToolbar()
+    }
+
+    private fun onClickToolbar() {
+        binding.toolbarCouponBox.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 
 }
