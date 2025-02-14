8dba951a112b4220ac6582069721ed98caae62a5
zzangse1 <zzangse1014@gmail.com>
2025-02-14 09:46:33 +0900

    Feat: 상태바 색상 설정

--- app/src/main/java/com/protect/jikigo/ui/travel/TravelSearchFragment.kt
+++ app/src/main/java/com/protect/jikigo/ui/travel/TravelSearchFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.protect.jikigo.R
 import com.protect.jikigo.databinding.FragmentTravelSearchBinding
+import com.protect.jikigo.ui.extensions.statusBarColor
 
 
 class TravelSearchFragment : Fragment() {
@@ -33,6 +35,11 @@ class TravelSearchFragment : Fragment() {
 
     private fun setLayout() {
         moveToTravel()
+        setStatusBar()
+    }
+
+    private fun setStatusBar() {
+        requireActivity().statusBarColor(R.color.white)
     }
 
     private fun moveToTravel() {
