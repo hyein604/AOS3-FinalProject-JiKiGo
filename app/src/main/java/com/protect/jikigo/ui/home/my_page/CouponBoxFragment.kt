fce2b16cce530e2db91788e74bac07ed2c5ae53d
bhd47 <bhd4734@gmail.com>
2025-02-14 10:20:58 +0900

    2025-02-14 첫번째 커밋 (버튼 색상 변경)

--- app/src/main/java/com/protect/jikigo/ui/home/my_page/CouponBoxFragment.kt
+++ app/src/main/java/com/protect/jikigo/ui/home/my_page/CouponBoxFragment.kt
@@ -5,18 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.protect.jikigo.R
+import com.protect.jikigo.databinding.FragmentCouponBoxBinding
+import com.protect.jikigo.databinding.FragmentPointHistoryBinding
+import com.protect.jikigo.ui.adapter.CouponBoxAdapter
+import com.protect.jikigo.ui.adapter.PointHistoryAdapter
+import com.protect.jikigo.ui.extensions.statusBarColor
 
 
 class CouponBoxFragment : Fragment() {
-
+    private var _binding: FragmentCouponBoxBinding? = null
+    private val binding get() = _binding!!
+    private val adapter: CouponBoxAdapter by lazy { CouponBoxAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coupon_box, container, false)
+    ): View {
+        _binding = FragmentCouponBoxBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setStatusBar()
+        setLayout()
+    }
+
+    private fun setStatusBar() {
+        requireActivity().statusBarColor(R.color.primary)
+    }
+
+    private fun setLayout() {
+        onClickToolbar()
+        recycler()
+    }
+
+    private fun onClickToolbar() {
+        binding.toolbarCouponBox.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun recycler() {
+        binding.recyclerCouponBox.adapter = adapter
     }
 
 }
