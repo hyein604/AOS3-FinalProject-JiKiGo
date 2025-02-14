9804456609e9186e22c955a69c8c08b5bb24ab14
zzangse1 <zzangse1014@gmail.com>
2025-02-14 09:42:47 +0900

    Feat: binding 설정, navigation 연결

--- app/src/main/java/com/protect/jikigo/ui/travel/TravelSearchFragment.kt
+++ app/src/main/java/com/protect/jikigo/ui/travel/TravelSearchFragment.kt
@@ -5,15 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.protect.jikigo.R
+import androidx.navigation.fragment.findNavController
+import com.protect.jikigo.databinding.FragmentTravelSearchBinding
 
 
 class TravelSearchFragment : Fragment() {
+    private var _binding: FragmentTravelSearchBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_travel_search, container, false)
+    ): View {
+        _binding = FragmentTravelSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setLayout()
+    }
+
+    private fun setLayout() {
+        moveToTravel()
+    }
+
+    private fun moveToTravel() {
+        binding.toobarSearch.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
