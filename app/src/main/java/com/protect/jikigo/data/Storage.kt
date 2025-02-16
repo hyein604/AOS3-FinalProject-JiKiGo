d65bc53864a7b18bc58d841f9b1e919e6118f658
zzangse1 <zzangse1014@gmail.com>
2025-02-17 01:29:56 +0900

    Update: 중괄호 추가

--- app/src/main/java/com/protect/jikigo/data/Storage.kt
+++ app/src/main/java/com/protect/jikigo/data/Storage.kt
@@ -24,50 +24,62 @@ object Storage {
 
     private fun getNotificationData(): List<Notification> {
         return listOf(
-            Notification("장세훈, '정세훈' 으로 이름 헷갈려 서러움 기념 이벤트\uD83C\uDF89","2025.2.7",
+            Notification(
+                "장세훈, '정세훈' 으로 이름 헷갈려 서러움 기념 이벤트\uD83C\uDF89", "2025.2.7",
                 "안녕하세요 지키GO 운영자 입니다.\n" +
-                "이번에 저희 팀원이 조장님의 이름을 헷갈렸습니다.\n" +
-                "성함이 장세훈 이신데 팀원분이 정세훈이라고 하셨어요.\n" +
-                "\n" +
-                "충격받으신 조장님이 기념으로 이벤트를 하나 기획 하셨습니다!\n" +
-                "\n" +
-                "무려 전 회원 10만 포인트 증정 이벤트입니다.\n" +
-                "단 사전 이벤트 신청하신 분만 해당됩니다.\n" +
-                "많이많이 참여해주세요~\n" +
-                "\n" +
-                "죄송합니다",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI4exT4h_yL8_DEigEIJObPdii4lJcZ12JRA&s"),
+                        "이번에 저희 팀원이 조장님의 이름을 헷갈렸습니다.\n" +
+                        "성함이 장세훈 이신데 팀원분이 정세훈이라고 하셨어요.\n" +
+                        "\n" +
+                        "충격받으신 조장님이 기념으로 이벤트를 하나 기획 하셨습니다!\n" +
+                        "\n" +
+                        "무려 전 회원 10만 포인트 증정 이벤트입니다.\n" +
+                        "단 사전 이벤트 신청하신 분만 해당됩니다.\n" +
+                        "많이많이 참여해주세요~\n" +
+                        "\n" +
+                        "죄송합니다",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI4exT4h_yL8_DEigEIJObPdii4lJcZ12JRA&s"
+            ),
 
-            Notification("도미노피자 제휴업체 선정","2025.2.7",
+            Notification(
+                "도미노피자 제휴업체 선정", "2025.2.7",
                 "안녕하세요 지키GO 운영자 입니다.\n" +
-                "여러분의 최애 음식은 무엇인가요?\n" +
-                "저의 최애 음식은 많은데요 그 중 하나는 피자입니다.\n" +
-                "\n" +
-                "도미노피자 제휴업체 맺어놨으니깐 피자 많이 드세요.\n" +
-                "전 진짜 많이 먹을거예요\n" +
-                "행복해여라~",
-                "https://cdn.dominos.co.kr/admin/upload/goods/20240326_Td6eyIV8.jpg?RS=350x350&SP=1"),
+                        "여러분의 최애 음식은 무엇인가요?\n" +
+                        "저의 최애 음식은 많은데요 그 중 하나는 피자입니다.\n" +
+                        "\n" +
+                        "도미노피자 제휴업체 맺어놨으니깐 피자 많이 드세요.\n" +
+                        "전 진짜 많이 먹을거예요\n" +
+                        "행복해여라~",
+                "https://cdn.dominos.co.kr/admin/upload/goods/20240326_Td6eyIV8.jpg?RS=350x350&SP=1"
+            ),
 
-            Notification("지키GO 환경부 선정 올해의 환경지킴이앱 선정","2025.2.7",
+            Notification(
+                "지키GO 환경부 선정 올해의 환경지킴이앱 선정", "2025.2.7",
                 "안녕하세요 지키GO 운영자 입니다.\n" +
-                "지키고가 환경부픽을 받았습니다\n" +
-                "지원금 달달하군요\n" +
-                "\n" +
-                "환경지킴이앱에 선정된건 모두 이용자분들 덕분입니다.\n" +
-                "저희가 만든 앱 잘 활용해 주셔서 감사해요.\n" +
-                "I love you..."),
+                        "지키고가 환경부픽을 받았습니다\n" +
+                        "지원금 달달하군요\n" +
+                        "\n" +
+                        "환경지킴이앱에 선정된건 모두 이용자분들 덕분입니다.\n" +
+                        "저희가 만든 앱 잘 활용해 주셔서 감사해요.\n" +
+                        "I love you..."
+            ),
 
-            Notification("공지사항 1","2025.2.7",
+            Notification(
+                "공지사항 1", "2025.2.7",
                 "안녕하세요 지키GO 운영자 입니다.\n" +
-                "검색 기능 테스트용 공지사항 1 입니다."),
+                        "검색 기능 테스트용 공지사항 1 입니다."
+            ),
 
-            Notification("중요공지","2025.2.7",
+            Notification(
+                "중요공지", "2025.2.7",
                 "안녕하세요 지키GO 운영자 입니다.\n" +
-                "검색 기능 테스트용 중요공지 입니다."),
+                        "검색 기능 테스트용 중요공지 입니다."
+            ),
 
-            Notification("새로운 업데이트","2025.2.7",
+            Notification(
+                "새로운 업데이트", "2025.2.7",
                 "안녕하세요 지키GO 운영자 입니다.\n" +
-                "검색 기능 테스트용 새로운 업데이트 입니다.")
+                        "검색 기능 테스트용 새로운 업데이트 입니다."
+            )
         )
 
     }
@@ -136,14 +148,16 @@ object Storage {
     private fun getRankingUserData(): List<RankingUser> {
         return listOf(
             RankingUser("천안지렁이라이더", "https://cdn.mhnse.com/news/photo/202409/320798_361970_3115.jpg", 18379),
-            RankingUser("구황작물", "https://wimg.heraldcorp.com/content/default/2024/04/12/20240412050389_0.jpg",182098),
-            RankingUser("감자무밨니", "https://i.namu.wiki/i/YTjOUCgGyUl7yza1MSfmi_RWJpkY4I2qflSY3NAp529euJDGe5hl1mneeHSTlBhz1LaQ2lwM2LohlguvqRdH7A.webp",142408),
+            RankingUser("구황작물", "https://wimg.heraldcorp.com/content/default/2024/04/12/20240412050389_0.jpg", 182098),
+            RankingUser("감자무밨니", "https://i.namu.wiki/i/YTjOUCgGyUl7yza1MSfmi_RWJpkY4I2qflSY3NAp529euJDGe5hl1mneeHSTlBhz1LaQ2lwM2LohlguvqRdH7A.webp", 142408),
             RankingUser("돌멩이", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-f-556Uod9bhUGfm-mvScCjEMn5Kx3AhinQ&s", 9865),
             RankingUser("울산안경도둑", "https://image.ajunews.com/content/image/2023/05/15/20230515143901746182.jpg", 24309),
             RankingUser("윈터아마멘터", "https://image.ajunews.com/content/image/2023/05/15/20230515143901746182.jpg", 1093)
-        ).sortedByDescending { it.walkCount 
-   }
-        
+        ).sortedByDescending {
+            it.walkCount
+        }
+    }
+
 
     private fun getFindAccountText(): List<String> {
         return listOf(
@@ -181,7 +195,7 @@ data class Coupon(
     val brand: String,
     val date: String,
     val image: Int,
-    val salesCount : Int
+    val salesCount: Int
 ) : Parcelable
 
 data class PointHistory(
