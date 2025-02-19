bcd7899ada582a846805697c7f88a26193b9c6b7
PSW <smiletor@gmail.com>
2025-02-19 10:34:26 +0900

    [Feat] Travel home banner

--- app/src/main/java/com/protect/jikigo/ui/travel/TravelHomeFragment.kt
+++ app/src/main/java/com/protect/jikigo/ui/travel/TravelHomeFragment.kt
@@ -1,27 +1,32 @@
 package com.protect.jikigo.ui.travel
 
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import com.protect.jikigo.R
 import com.protect.jikigo.data.Coupon
 import com.protect.jikigo.data.Storage
 import com.protect.jikigo.databinding.FragmentTravelHomeBinding
 import com.protect.jikigo.ui.adapter.CouponAdaptor
+import com.protect.jikigo.ui.adapter.TravelBannerAdapter
 import com.protect.jikigo.ui.adapter.TravelCouponOnClickListener
+import com.protect.jikigo.ui.extensions.applySpannableStyles
 import com.protect.jikigo.ui.extensions.statusBarColor
 
 class TravelHomeFragment : Fragment(), TravelCouponOnClickListener {
     private var _binding: FragmentTravelHomeBinding? = null
     private val binding get() = _binding!!
+
+    private var currentPage = 0
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,8 +49,9 @@ class TravelHomeFragment : Fragment(), TravelCouponOnClickListener {
     private fun setLayout() {
         setStatusBar()
         moveToSearch()
+        setBannerSlider()
         moveToHotCoupon()
-        setTextWithColor(binding.tvTravelHomeHotCoupon, "HOT 추천쿠폰", "HOT", R.color.primary)
+        setText()
         setRecyclerView()
     }
 
@@ -72,22 +78,95 @@ class TravelHomeFragment : Fragment(), TravelCouponOnClickListener {
         }
     }
 
+    private fun setBannerSlider(){
+        val bannerImages = listOf(
+            "https://lh3.googleusercontent.com/proxy/iA2VKsfQq-DYS_Axoz4zkjw_fnijbMtOMCc7ZxFXgV5RYyQ9wZS_JFZZ2ypzdyI3ZUJQpbE8a7yFHQAXR-lycYe2HhneOZdWOGJIU_Ysc5m-0H99kDVKCSLnNMeFng",
+            "https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/506341c9-1d0e-4f41-9081-40f6c9fbb4ec.jpeg",
+            "https://i.namu.wiki/i/xN8ho4RHhhOntPkg6lUzKqkUmIvwARA0KzMjv8xZm9hP-T64alryJs4APV255xFBnoL74ea0RwWurso8PvnCSw.webp"
+        )
+
+        val adaptor = TravelBannerAdapter(bannerImages) { position ->
+            val actualPosition =  (position - 1 + bannerImages.size) % bannerImages.size
+            when (position) {
+                0 -> {
+                    Toast.makeText(requireContext(), "$actualPosition 번째 배너", Toast.LENGTH_SHORT).show()
+                }
+                1 -> {
+                    Toast.makeText(requireContext(), "$actualPosition 번째 배너", Toast.LENGTH_SHORT).show()
+                }
+                2 -> {
+                    Toast.makeText(requireContext(), "$actualPosition 번째 배너", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+
+                }
+            }
+        }
+        binding.vpTravelBanner.adapter = adaptor
+
+        TabLayoutMediator(binding.indicatorTravelBanner, binding.vpTravelBanner) { tab, position ->
+            // 실제 데이터 개수 (더미 제외)
+            val itemCount = bannerImages.size
+
+            if (position == 0 || position == itemCount + 1) {
+                // 첫 번째와 마지막(더미 데이터) 인디케이터 숨기기
+                tab.view.visibility = View.GONE
+            } else {
+                tab.view.visibility = View.VISIBLE
+            }
+        }.attach()
+
+        startAutoSlide()
+
+        binding.vpTravelBanner.setCurrentItem(1, false)
+
+        binding.vpTravelBanner.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                val itemCount = binding.vpTravelBanner.adapter?.itemCount ?: return
+
+                binding.vpTravelBanner.postDelayed({
+                    when (position) {
+                        0 -> binding.vpTravelBanner.setCurrentItem(itemCount - 2, false) // 첫 번째 더미 → 마지막 아이템
+                        itemCount - 1 -> binding.vpTravelBanner.setCurrentItem(1, false) // 마지막 더미 → 첫 번째 아이템
+                    }
+                }, 300)
+            }
+        })
+    }
+
+    private fun startAutoSlide() {
+        val itemCount = binding.vpTravelBanner.adapter?.itemCount ?: return
+
+        val slideRunnable = object : Runnable {
+            override fun run() {
+                val nextPage = binding.vpTravelBanner.currentItem + 1
+                if (nextPage == itemCount) {
+                    // 마지막 더미에 도달하면 첫 번째 실제 아이템으로 이동
+                    binding.vpTravelBanner.setCurrentItem(0, true)
+                } else {
+                    // 다음 페이지로 이동
+                    binding.vpTravelBanner.setCurrentItem(nextPage, true)
+                }
+                handler.postDelayed(this, 3000)
+            }
+        }
+
+        // 슬라이드 시작
+        handler.postDelayed(slideRunnable, 3000)
+    }
+
     private fun setRecyclerView(){
         val coupon = Storage.coupon.sortedByDescending { it.salesCount }.take(4)
         val adapter = CouponAdaptor(coupon, this)
         binding.rvHotCouponList.adapter = adapter
     }
 
-    private fun setTextWithColor(textView: TextView, text: String, target: String, colorResId: Int) {
-        val spannableString = SpannableString(text).apply {
-            val start = text.indexOf(target)
-            val end = start + target.length
-            setSpan(
-                ForegroundColorSpan(ContextCompat.getColor(requireContext(), colorResId)),
-                start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-        }
-        textView.text = spannableString
+    private fun setText() {
+        binding.tvTravelHomeHotCoupon.applySpannableStyles(
+            0, 3, R.color.primary
+        )
     }
 
     override fun onClickListener(item: Coupon) {
