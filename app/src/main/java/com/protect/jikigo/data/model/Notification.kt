a4bbb44737ac9eec115bf56e3c9fe4962a0edb4e
hyeinLion <hyein604@naver.com>
2025-02-20 22:27:53 +0900

    Feat: 공지사항 MVVM 구조로 변경, 힐트 주입

---
+++ app/src/main/java/com/protect/jikigo/data/model/Notification.kt
@@ -0,0 +1,12 @@
+package com.protect.jikigo.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Notification(
+    val title: String ="",
+    val date: String="",
+    val content: String="",
+    val image: String? = null
+) : Parcelable
