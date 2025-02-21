a1268a37493a4bb1e7701887ff910aa36c14784d
zzangse1 <zzangse1014@gmail.com>
2025-02-21 17:05:53 +0900

    Feat: DataStore 설정

--- app/src/main/java/com/protect/jikigo/App.kt
+++ app/src/main/java/com/protect/jikigo/App.kt
@@ -1,14 +1,24 @@
 package com.protect.jikigo
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.google.firebase.FirebaseApp
 import com.google.firebase.appcheck.FirebaseAppCheck
 import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "kakao")
 
 @HiltAndroidApp
 class App : Application() {
@@ -16,14 +26,26 @@ class App : Application() {
         super.onCreate()
         // Firebase 초기화
         FirebaseApp.initializeApp(this)
-        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
-        Log.d("KAKAO", "${Utility.getKeyHash(this)}")
-
-        // Firebase App Check 활성화
-        val firebaseAppCheck = FirebaseAppCheck.getInstance()
-        firebaseAppCheck.installAppCheckProviderFactory(
+        FirebaseAppCheck.getInstance().installAppCheckProviderFactory(
             PlayIntegrityAppCheckProviderFactory.getInstance()
         )
 
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+        Log.d("KAKAO", "${Utility.getKeyHash(this)}")
+    }
+
+    companion object {
+        private val USER_ID = stringPreferencesKey("KAKAO")
+        suspend fun setUserId(context: Context, userId: String) {
+            context.dataStore.edit { preferences ->
+                preferences[USER_ID] = userId
+            }
+        }
+
+        fun getUserId(context: Context): Flow<String?> {
+            return context.dataStore.data.map { preferences ->
+                preferences[USER_ID]
+            }
+        }
     }
 }
