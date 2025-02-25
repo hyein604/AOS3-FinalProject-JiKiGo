cfd76b0e634c50db1302acaefe33b9827abb52f0
hyeinLion <hyein604@naver.com>
2025-02-25 17:24:55 +0900

    Remove: 안쓰는 함수 삭제

--- app/src/main/java/com/protect/jikigo/ui/viewModel/RankingViewModel.kt
+++ app/src/main/java/com/protect/jikigo/ui/viewModel/RankingViewModel.kt
@@ -30,7 +30,6 @@ class RankingViewModel @Inject constructor(
 
     init {
         fetchRankingData()
-        scheduleDailyStepUpdate()
     }
 
     private fun fetchRankingData() {
@@ -53,63 +52,5 @@ class RankingViewModel @Inject constructor(
                 Log.e("HomeViewModel", "Error fetching user info", e)
             }
         }
-    }
-
-    private fun scheduleDailyStepUpdate() {
-        val handler = Handler(Looper.getMainLooper())
-        val runnable = object : Runnable {
-            override fun run() {
-                updateUserSteps()
-                handler.postDelayed(this, 24 * 60 * 60 * 1000) // 하루마다 실행
-            }
-        }
-        val delay = getMillisUntilMidnight()
-        handler.postDelayed(runnable, delay)
-    }
-
-    private fun updateUserSteps() {
-        viewModelScope.launch {
-            val userId = _item.value?.userId ?: return@launch
-            val userInfo = rankingRepo.getUserInfo(userId) ?: return@launch
-
-            // 이번 주 월요일 체크 후, 필요하면 초기화
-            resetWeeklyStepsIfNeeded(userInfo)
-
-            // 오늘 걸음수를 userStepDaily에 저장
-            val newDailyStep = fetchTodayStepsFromSamsungHealth() // 삼성 헬스에서 걸음 수 가져오기
-            val newWeeklyStep = userInfo.userStepWeekly + newDailyStep
-
-            val updatedUser = userInfo.copy(
-                userStepDaily = newDailyStep,
-                userStepWeekly = newWeeklyStep
-            )
-
-            rankingRepo.updateUserInfo(updatedUser)
-            _item.postValue(updatedUser)
-        }
-    }
-
-    private suspend fun resetWeeklyStepsIfNeeded(userInfo: UserInfo) {
-        val calendar = Calendar.getInstance()
-        if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
-            val resetUser = userInfo.copy(userStepWeekly = 0)
-            rankingRepo.updateUserInfo(resetUser)
-            _item.postValue(resetUser)
-        }
-    }
-
-    private fun getMillisUntilMidnight(): Long {
-        val calendar = Calendar.getInstance().apply {
-            set(Calendar.HOUR_OF_DAY, 23)
-            set(Calendar.MINUTE, 59)
-            set(Calendar.SECOND, 0)
-            set(Calendar.MILLISECOND, 0)
-        }
-        return calendar.timeInMillis - System.currentTimeMillis()
-    }
-
-    private fun fetchTodayStepsFromSamsungHealth(): Int {
-        // TODO: 삼성 헬스 API 연동 후 데이터 가져오는 코드 추가
-        return 0
     }
 }
