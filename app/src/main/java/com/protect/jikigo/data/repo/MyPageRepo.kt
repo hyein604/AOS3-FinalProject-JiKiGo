71d80dcb5762d18d19f20e61789499406268d235
bhd47 <bhd4734@gmail.com>
2025-02-25 17:47:42 +0900

    2025-02-25 커밋 (프로필 사진 지우기)

--- app/src/main/java/com/protect/jikigo/data/repo/MyPageRepo.kt
+++ app/src/main/java/com/protect/jikigo/data/repo/MyPageRepo.kt
@@ -4,14 +4,16 @@ import android.net.Uri
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.protect.jikigo.data.model.UserInfo
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class MyPageRepo @Inject constructor(
     private val firestore: FirebaseFirestore,
+    private val storage: FirebaseStorage
 ) {
 
     fun uploadProfileImage(userId: String, uri: Uri, onSuccess: (String) -> Unit, onFailure: (Exception) -> Unit) {
-        val storageReference = FirebaseStorage.getInstance().reference
+        val storageReference = storage.reference
         val profileImageRef = storageReference.child("profile_images/${userId}_profile.jpg")
         profileImageRef.putFile(uri)
             .addOnSuccessListener {
@@ -22,6 +24,14 @@ class MyPageRepo @Inject constructor(
             .addOnFailureListener { exception ->
                 onFailure(exception)
             }
+    }
+
+    fun removeProfileImage(userId: String, onSuccess: (Boolean) -> Unit) {
+        val storageReference = storage.reference
+        val profileImageRef = storageReference.child("profile_images/${userId}_profile.jpg")
+        profileImageRef.delete()
+            .addOnSuccessListener { onSuccess(true) }
+            .addOnFailureListener { onSuccess(false) }
     }
 
     fun getProfile(userId: String, onResult: (UserInfo?, String?) -> Unit) {
