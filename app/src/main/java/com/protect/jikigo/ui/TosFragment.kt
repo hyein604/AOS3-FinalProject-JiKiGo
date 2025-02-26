a5f4690d7a134bca18bbe0eeec88a50f3c826c9b
PSW <smiletor@gmail.com>
2025-02-26 17:39:40 +0900

    [Feat] bottom sheet to tos

--- app/src/main/java/com/protect/jikigo/ui/TosFragment.kt
+++ app/src/main/java/com/protect/jikigo/ui/TosFragment.kt
@@ -1,60 +1,57 @@
 package com.protect.jikigo.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.activity.addCallback
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.protect.jikigo.R
+import com.protect.jikigo.databinding.FragmentTosBinding
+import com.protect.jikigo.ui.travel.TravelPaymentBottomSheetFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TosFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TosFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentTosBinding? = null
+    private val binding get() = _binding!!
+    private val args: WebViewFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tos, container, false)
+    ): View {
+        _binding = FragmentTosBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setLayout()
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            findNavController().popBackStack()
+            findNavController().navigate(R.id.action_travelCouponDetail_to_travelPaymentBottomSheet)
+        }
+    }
+
+    private fun setLayout() {
+        moveToWebSite()
+    }
+
+    private fun moveToWebSite() {
+        binding.webViewTos.settings.javaScriptEnabled = true
+        binding.webViewTos.webViewClient = WebViewClient()
+        binding.webViewTos.loadUrl(args.urlMethod.url)
+        Log.d("TEST", args.urlMethod.url)
+    }
+
 }
