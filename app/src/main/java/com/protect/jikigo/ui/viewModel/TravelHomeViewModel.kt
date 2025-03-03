35242689459ded4afd81a43e8ed5d098b9525dcd
PSW <smiletor@gmail.com>
2025-03-03 11:33:11 +0900

    Feat: Add hot coupon fab, change banner image

--- app/src/main/java/com/protect/jikigo/ui/viewModel/TravelHomeViewModel.kt
+++ app/src/main/java/com/protect/jikigo/ui/viewModel/TravelHomeViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.protect.jikigo.R
 import com.protect.jikigo.data.model.Coupon
 import com.protect.jikigo.data.repo.CouponRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,8 +19,8 @@ class TravelHomeViewModel @Inject constructor(
     private val _hotCoupons = MutableLiveData<List<Coupon>>()
     val hotCoupons: LiveData<List<Coupon>> get() = _hotCoupons
 
-    private val _bannerImages = MutableLiveData<List<String>>()
-    val bannerImages: LiveData<List<String>> get() = _bannerImages
+    private val _bannerImages = MutableLiveData<List<Int>>()
+    val bannerImages: LiveData<List<Int>> get() = _bannerImages
 
     init {
         fetchHotCoupons()
@@ -35,9 +36,9 @@ class TravelHomeViewModel @Inject constructor(
 
     private fun loadBannerImages() {
         _bannerImages.value = listOf(
-            "https://lh3.googleusercontent.com/proxy/iA2VKsfQq-DYS_Axoz4zkjw_fnijbMtOMCc7ZxFXgV5RYyQ9wZS_JFZZ2ypzdyI3ZUJQpbE8a7yFHQAXR-lycYe2HhneOZdWOGJIU_Ysc5m-0H99kDVKCSLnNMeFng",
-            "https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/506341c9-1d0e-4f41-9081-40f6c9fbb4ec.jpeg",
-            "https://i.namu.wiki/i/xN8ho4RHhhOntPkg6lUzKqkUmIvwARA0KzMjv8xZm9hP-T64alryJs4APV255xFBnoL74ea0RwWurso8PvnCSw.webp"
+            R.drawable.img_travel_banner1,
+            R.drawable.img_travel_banner2,
+            R.drawable.img_travel_banner3
         )
     }
 }
