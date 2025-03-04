b9436d5f1b847afcce2bb36ae7bff436475e821d
zzangse1 <zzangse1014@gmail.com>
2025-03-04 13:41:28 +0900

    Chore: 사용하지 않는 import제거

--- app/src/main/java/com/protect/jikigo/App.kt
+++ app/src/main/java/com/protect/jikigo/App.kt
@@ -1,12 +1,10 @@
 package com.protect.jikigo
 
 import android.app.Application
-import android.util.Log
 import com.google.firebase.FirebaseApp
 import com.google.firebase.appcheck.FirebaseAppCheck
 import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 import com.kakao.sdk.common.KakaoSdk
-import com.kakao.sdk.common.util.Utility
 import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.HiltAndroidApp
 
